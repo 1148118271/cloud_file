32f74fa571ce9f352229359fab903123e2f44dcf
gaoxiangkang <1148118271@qq.com>
2022-09-07 23:16:36 +0800

    first commit

--- src/html.rs
+++
@@ -1,8 +0,0 @@
-#[warn(dead_code)]
-pub const PAGE_403 : &str = r#"<html>
-<head><title>403 Forbidden</title></head>
-<body>
-<center><h1>403 Allow only Get requests</h1></center>
-<hr><center>nginx/1.22.0</center>
-</body>
-</html>"#;
