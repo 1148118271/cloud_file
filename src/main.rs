205cc2c1b2787f292f080fea0fc8de0f3684a949
gaoxiangkang <1148118271@qq.com>
2022-09-07 23:19:56 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,111 @@
-mod request;
-mod html;
-mod response;
+use std::fs;
+use std::path::Path;
+use toml::Value;
 
-use std::io::{ErrorKind, Write};
-use std::thread;
-use std::time::Duration;
-use tokio::io::AsyncReadExt;
-use tokio::net::{TcpListener, TcpStream};
-use crate::request::Request;
-
-#[tokio::main]
-async fn main() {
-    server_run().await;
+fn main() {
+    let args = Args::new();
+    println!("file upload [{}] => [{}]", args.lf.as_str(), args.c.remote_path().as_str());
+    let mut session = ssh_rs::ssh::create_session();
+    session.set_user_and_password(args.c.user(), args.c.password());
+    let addr = format!("{}:{}", args.c.ip(), args.c.port());
+    session.connect(addr).unwrap();
+    let scp = session.open_scp().unwrap();
+    scp.upload(args.lf.as_str(), args.c.remote_path().as_str()).unwrap();
+    session.close().unwrap();
+    println!("file upload successfully.");
 }
 
 
-async fn server_run() {
-    let std_listener = std::net::TcpListener::bind("0.0.0.0:9258").unwrap();
-    std_listener.set_nonblocking(true).unwrap();
-    let listener = TcpListener::from_std(std_listener).unwrap();
 
-    loop {
-        let (server, addr) = listener.accept().await.unwrap();
-        println!("client addr {}", addr);
-        let request = Request::process(server).await;
-        println!("{:?}", request);
+#[derive(Debug)]
+struct Args {
+    c: Conf,
+    lf: String,
+}
+
+impl Args  {
+    pub fn new() -> Self {
+        let mut args = std::env::args();
+        let exe_path = args.next().unwrap();
+        let p = Path::new(&exe_path);
+        let c = p.parent().unwrap().parent().unwrap();
+        let mut path_buf = c.join("config").join("conf.toml");
+        let mut lf = String::new();
+
+        if args.len() <= 0 {
+            let conf = Conf::from(path_buf.as_path());
+            lf = conf.local_path();
+            return Args {
+                c: conf,
+                lf
+            }
+        }
+
+        loop {
+            let arg = args.next();
+            if arg.is_none() {
+                break;
+            }
+            match arg.unwrap().as_str() {
+                "c" => {
+                    if let Some(p) = args.next() {
+                        path_buf = Path::new(p.as_str()).to_path_buf()
+                    }
+                },
+                "lf" => {
+                    if let Some(lfs) = args.next() {
+                        lf = lfs;
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        let conf = Conf::from(path_buf.as_path());
+        if lf.is_empty() {
+            lf = conf.local_path();
+        }
+         Args {
+            c: conf,
+            lf
+         }
     }
 }
 
 
-#[test] fn t() {
-    let mut stream = std::net::TcpStream::connect("127.0.0.1:9258").unwrap();
-    let mut c = vec![];
-    for i in 0..255 {
-        c.push(i);
+#[derive(Debug)]
+struct Conf {
+    v: Value
+}
+
+impl Conf {
+    fn from(path: &Path) -> Self {
+        let ts = fs::read_to_string(path.to_path_buf()).unwrap();
+        let v: Value = toml::from_str(ts.as_str()).unwrap();
+        Conf { v }
     }
-    stream.write(&c).unwrap();
-    thread::sleep(Duration::from_secs(60 * 60 * 12));
+    fn ip(&self) -> String {
+        let ip = self.v.get("ip").unwrap();
+        ip.as_str().unwrap().to_string()
+    }
+    fn port(&self) -> u32 {
+        let port = self.v.get("port").unwrap();
+        port.as_integer().unwrap() as u32
+    }
+    fn user(&self) -> String {
+        let user = self.v.get("user").unwrap();
+        user.as_str().unwrap().to_string()
+    }
+    fn password(&self) -> String {
+        let password = self.v.get("password").unwrap();
+        password.as_str().unwrap().to_string()
+    }
+    fn local_path(&self) -> String {
+        let local_path = self.v.get("local_path").unwrap();
+        local_path.as_str().unwrap().to_string()
+    }
+    fn remote_path(&self) -> String {
+        let remote_path = self.v.get("remote_path").unwrap();
+        remote_path.as_str().unwrap().to_string()
+    }
 }
